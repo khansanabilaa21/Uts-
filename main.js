a951da7159699336d8de2a85d27cbe29262f1595
khansanabilaa21 <khansanabilaa21@upi.edu>
2023-10-26 17:18:03 +0300

    add index.html

---
+++ main.js
@@ -0,0 +1,42 @@
+.navbar {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    font-family: sans-serif;
+  }
+
+  .navbar a {
+    text-decoration: none;
+    margin: 0 10px; 
+  }
+
+  .h1 {
+    text-align: left;
+    font-family: sans-serif;
+  }
+  
+  .home-content {
+    text-align: left;
+    padding: 100px;
+}
+
+.profile-image {
+    max-width: 50%;
+    border-radius: 0%;
+    position: center;
+}
+
+.name {
+    font-size: 30px;
+    margin: 20px 0;
+    text-align: left;
+}
+
+.resume-button {
+    background-color: #000000; 
+    color: #fff; 
+    padding: 10px 10px;
+    font-size: 16px;
+    text-decoration: none;
+    position: left;
+}
